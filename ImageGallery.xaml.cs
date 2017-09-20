73680c32fc76698d2f5b27a650960424ba9552cb
spagni <spagni@kimn.com.ar>
2017-09-20 12:19:48 -0300

    Ejercicios del curso de Xamarin

---
+++ ImageGallery.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorldPCL
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImageGallery : ContentPage
+	{
+        private int currentImageId = 1;
+
+        public ImageGallery ()
+		{
+			InitializeComponent ();
+            currentImageId = 1;
+            loadImage();
+		}
+
+        void loadImage()
+        {
+            image.Source = new UriImageSource
+            {
+                Uri = new Uri(String.Format("http://lorempixel.com/1920/1080/city/{0}", currentImageId)),
+                CachingEnabled = false
+            };
+        }
+
+        void Previous_Clicked(object sender, System.EventArgs e)
+        {
+            currentImageId--;
+            if (currentImageId == 0)
+                currentImageId = 10;
+
+            loadImage();
+        }
+
+        void Next_Clicked(object sender, System.EventArgs e)
+        {
+            currentImageId++;
+            if (currentImageId == 10)
+                currentImageId = 0;
+
+            loadImage();
+        }
+
+    }
+}
---
+++ QuotePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorldPCL
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotePage : ContentPage
+    {
+        private int _index = 0;
+        private string[] quotes = new string[] { "a", "b", "c", "d", "e", "f" };
+        public QuotePage()
+        {
+            InitializeComponent();
+            currentQuote.Text = quotes[_index];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _index++;
+            if (_index >= quotes.Length) _index = 0;
+            currentQuote.Text = quotes[_index];
+        }
+    }
+}
